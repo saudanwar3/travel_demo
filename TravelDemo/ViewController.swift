447ef5624ea0e1b8dcc962e777ef517b216a0c31
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-03-09 21:42:54 +0400

    Initial Commit

---
+++ TravelDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelDemo
+//
+//  Created by Muhammad Saud Anwar on 09/03/2021.
+//  Copyright © 2021 Muhammad Saud Anwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TravelDemoTests/TravelDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TravelDemoTests.swift
+//  TravelDemoTests
+//
+//  Created by Muhammad Saud Anwar on 09/03/2021.
+//  Copyright © 2021 Muhammad Saud Anwar. All rights reserved.
+//
+
+import XCTest
+@testable import TravelDemo
+
+class TravelDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
