caeb9fe43b9f99464cb0dd37856ebe3c732aa2ae
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-03-10 06:21:17 +0400

    Added functionalities

--- TravelDemo/Controllers/ViewController.swift
+++ TravelDemo/Controllers/ViewController.swift
@@ -9,36 +9,195 @@
 import UIKit
 import SwiftyJSON
 class ViewController: UIViewController {
-
-    var flightModel:FlightDataModel?
     
+    // MARK: IBOUTLETS
+    
+    @IBOutlet weak var buttonsStackView: UIStackView!
+    
+    @IBOutlet weak var cheapestFlightMainView: UIView!
+    @IBOutlet weak var cheapestFlightIconImgView: UIImageView!
+    @IBOutlet weak var cheapestFlightLbl: UILabel!
+    @IBOutlet weak var cheapestFlightBtn: UIButton!
+    
+    @IBOutlet weak var fastFlightMainView: UIView!
+    @IBOutlet weak var fastFlightIconImgView: UIImageView!
+    @IBOutlet weak var fastFlightLbl: UILabel!
+    @IBOutlet weak var fastFlightBtn: UIButton!
+    
+    @IBOutlet weak var flexibleMainView: UIView!
+    @IBOutlet weak var flexibleIconImgView: UIImageView!
+    @IBOutlet weak var flexibleLbl: UILabel!
+    @IBOutlet weak var flexibleBtn: UIButton!
+    
+    @IBOutlet weak var flightCollectionView: UICollectionView!
+    
+    // MARK: Variables
+    var flightModel:FlightDataModel?
+    let inset: CGFloat = 10
+    let minimumLineSpacing: CGFloat = 10
+    let minimumInteritemSpacing: CGFloat = 10
+    let cellsPerRow = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.initializeView()
         self.loadJsonfile()
+        self.cheapestFlightBtnClicked(self.cheapestFlightBtn)
     }
-
     
+    func initializeView() -> Void {
+        
+        self.flightCollectionView.delegate = self
+        self.flightCollectionView.dataSource = self
+        let flightCollectionCell = UINib.init(nibName: String(describing: FlightCollectionViewCell.self), bundle: nil)
+        self.flightCollectionView.register(flightCollectionCell, forCellWithReuseIdentifier: String(describing: FlightCollectionViewCell.self))
+        
+        self.cheapestFlightMainView.backgroundColor = .white
+        self.cheapestFlightLbl.textColor = .darkGray
+        cheapestFlightIconImgView.image = cheapestFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+        cheapestFlightIconImgView.tintColor = UIColor.darkGray
+        
+        self.fastFlightMainView.backgroundColor = .white
+        self.fastFlightLbl.textColor = .darkGray
+        fastFlightIconImgView.image = fastFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+        fastFlightIconImgView.tintColor = UIColor.darkGray
+        
+        self.flexibleMainView.backgroundColor = .white
+        self.flexibleLbl.textColor = .darkGray
+        flexibleIconImgView.image = flexibleIconImgView.image?.withRenderingMode(.alwaysTemplate)
+        flexibleIconImgView.tintColor = UIColor.darkGray
+        
+        
+    }
     func loadJsonfile() -> Void {
         self.flightModel = self.loadJson(fileName: "SearchList")!
         print(self.flightModel?.results.first?.origin ?? "")
+        self.flightCollectionView.reloadData()
     }
+    
+    // MARK: Button Actions
+    @IBAction func cheapestFlightBtnClicked(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0.1, options: .curveLinear, animations: {
+            self.cheapestFlightMainView.backgroundColor = .white
+            self.cheapestFlightLbl.textColor = .link
+            self.cheapestFlightIconImgView.image = self.cheapestFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.cheapestFlightIconImgView.tintColor = UIColor.link
+            
+            self.fastFlightMainView.backgroundColor = .white
+            self.fastFlightLbl.textColor = .darkGray
+            self.fastFlightIconImgView.image = self.fastFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.fastFlightIconImgView.tintColor = UIColor.darkGray
+            
+            self.flexibleMainView.backgroundColor = .white
+            self.flexibleLbl.textColor = .darkGray
+            self.flexibleIconImgView.image = self.flexibleIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.flexibleIconImgView.tintColor = UIColor.darkGray
+        }) { _ in
+        }
+        
+        
+    }
+    
+    @IBAction func fastFlightBtnClicked(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.2, delay: 0.1, options: .curveLinear, animations: {
+            self.fastFlightMainView.backgroundColor = .white
+            self.fastFlightLbl.textColor = .link
+            self.fastFlightIconImgView.image = self.fastFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.fastFlightIconImgView.tintColor = UIColor.link
+            
+            self.cheapestFlightMainView.backgroundColor = .white
+            self.cheapestFlightLbl.textColor = .darkGray
+            self.cheapestFlightIconImgView.image = self.cheapestFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.cheapestFlightIconImgView.tintColor = UIColor.darkGray
+            
+            self.flexibleMainView.backgroundColor = .white
+            self.flexibleLbl.textColor = .darkGray
+            self.flexibleIconImgView.image = self.flexibleIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.flexibleIconImgView.tintColor = UIColor.darkGray
+        }) { _ in
+        }
+        
+        
+        
+    }
+    
+    @IBAction func flexibleFlightBtnClicked(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0.1, options: .curveLinear, animations: {
+            self.flexibleMainView.backgroundColor = .white
+            self.flexibleLbl.textColor = .link
+            self.flexibleIconImgView.image = self.flexibleIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.flexibleIconImgView.tintColor = UIColor.link
+            
+            self.cheapestFlightMainView.backgroundColor = .white
+            self.cheapestFlightLbl.textColor = .darkGray
+            self.cheapestFlightIconImgView.image = self.cheapestFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.cheapestFlightIconImgView.tintColor = UIColor.darkGray
+            
+            self.fastFlightMainView.backgroundColor = .white
+            self.fastFlightLbl.textColor = .darkGray
+            self.fastFlightIconImgView.image = self.fastFlightIconImgView.image?.withRenderingMode(.alwaysTemplate)
+            self.fastFlightIconImgView.tintColor = UIColor.darkGray
+        }) { _ in
+        }
+        
+    }
+    
+    
+}
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.flightModel?.results.count)!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String.init(describing: FlightCollectionViewCell.self), for: indexPath) as!  FlightCollectionViewCell
+        let airlineDetails = self.flightModel?.results[indexPath.row].segments.first?.airlineDetails
+        var airlineCode:String = ""
+        var flightNum:String = ""
+        
+        if airlineDetails == nil {
+            airlineCode = "N/A"
+            flightNum = ""
+        }
+        else
+        {
+            airlineCode = (self.flightModel?.results[indexPath.row].segments.first?.airlineDetails.airlineCode)! + " - "
+            flightNum = (self.flightModel?.results[indexPath.row].segments.first?.airlineDetails.flightNumber)!
+
+        }
+        cell.flightNumLbl.text = airlineCode + flightNum
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let marginsAndInsets = inset * 2 + collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right + minimumInteritemSpacing * CGFloat(cellsPerRow - 1)
+        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
+        let itemHeight = itemWidth / 2.5 //ratio
+
+        return CGSize(width: itemWidth, height: itemHeight )
+
+    }
 }
 
 extension ViewController
 {
     func loadJson(fileName: String) -> FlightDataModel? {
-       guard
+        guard
             let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
             let data = try? Data(contentsOf: url),
             let dictionary = try? JSON(data),
             let flightData = try? FlightDataModel(fromJson: dictionary)
-       else {
+            else {
+                
+                return nil
+        }
         
-            return nil
-       }
-
-       return flightData
+        return flightData
     }
-
+    
 }
--- TravelDemo/XibWithClasses/FlightCollectionViewCell/FlightCollectionViewCell.swift
+++ TravelDemo/XibWithClasses/FlightCollectionViewCell/FlightCollectionViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FlightCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var flightNumLbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
