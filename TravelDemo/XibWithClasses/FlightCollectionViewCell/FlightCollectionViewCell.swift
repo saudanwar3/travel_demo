7cf2bf4c94c237d0bc85f61c087ea59bcd3703fe
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-03-10 05:08:50 +0400

    Added files

---
+++ TravelDemo/XibWithClasses/FlightCollectionViewCell/FlightCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FlightCollectionViewCell.swift
+//  TravelDemo
+//
+//  Created by Muhammad Saud Anwar on 10/03/2021.
+//  Copyright © 2021 Muhammad Saud Anwar. All rights reserved.
+//
+
+import UIKit
+
+class FlightCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
