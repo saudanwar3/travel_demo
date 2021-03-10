039fc1357b169f70f2aca31611552357f6ed9ae1
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-03-10 20:33:46 +0400

    Added files

---
+++ TravelDemo/Utilities/UIImageExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageExtension.swift
+//  TravelDemo
+//
+//  Created by Muhammad Saud Anwar on 10/03/2021.
+//  Copyright © 2021 Muhammad Saud Anwar. All rights reserved.
+//
+
+import Foundation
