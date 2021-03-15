a30888cb14e0577aa0d2c589625b9bfe17309c15
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-03-15 06:40:35 +0400

    new files added

---
+++ TravelDemo/Controllers/FlightDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FlightDetailsViewController.swift
+//  TravelDemo
+//
+//  Created by Muhammad Saud Anwar on 15/03/2021.
+//  Copyright © 2021 Muhammad Saud Anwar. All rights reserved.
+//
+
+import UIKit
+
+class FlightDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
